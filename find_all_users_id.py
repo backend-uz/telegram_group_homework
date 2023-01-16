b65e518871fc7be5c609f13464fca87fc295e36b
user1 <backenduz@gmail.com>
2023-01-17 00:31:04 +0500

    ids

--- find_all_users_id.py
+++ find_all_users_id.py
@@ -13,8 +13,9 @@ def find_all_users_id(data: dict)->list:
     data = data['messages']
     for i in data:
         if 'actor_id' in i:
-            if i['actor_id'] not in users:
-                users.append(i['actor_id'])
+            if i['actor_id'] not in 'channel1640165484':
+                if i['actor_id'] not in users:
+                    users.append(i['actor_id'])
         # if 'from_id' in i:
         #     if i['from_id'] not in users:
         #         users.append(i['from_id'])
