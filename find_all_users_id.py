c3d2e7f1062ebdfa711a26f0ec05a51d9c44848b
user1 <backenduz@gmail.com>
2023-01-16 23:17:03 +0500

    ids

--- find_all_users_id.py
+++ find_all_users_id.py
@@ -12,7 +12,7 @@ def find_all_users_id(data: dict)->list:
     users = []
     data = data['messages']
     for i in data:
-        if 'from_id' in i:
-            users.append(i['from_id'])
+        if 'actor_id' in i:
+            users.append(i['actor_id'])
     return users
 print(find_all_users_id(read_data('data/result.json')))
